55ab8493f8e97305e93df95d6984f82fd96c7d6e
Rogério Rocha <rogeriorocha@Rogerios-MacBook-Air.local>
2024-03-06 07:45:27 -0300

    function init

--- script.js
+++ script.js
@@ -8,12 +8,7 @@ const currentScoreP2 = document.getElementById('current--1');
 const player1 = document.querySelector('.player--0');
 const player2 = document.querySelector('.player--1');
 
-totalScoreP1.textContent = 0;
-totalScoreP2.textContent = 0;
-dice.classList.add('hidden');
-let curScore = 0;
-let totalScore1 = 0;
-let totalScore2 = 0;
+let curScore, totalScore1, totalScore2;
 
 // FUNCTIONS
 const switchPlayer = function () {
@@ -34,11 +29,11 @@ const verifyPlayerWinner = function () {
   if (totalScore1 >= 100) {
     player1.classList.add('player--winner');
     player2.classList.remove('player--active');
-    player1.classList.add('player--active');
+    player1.classList.remove('player--active');
   } else if (totalScore2 >= 100) {
     player2.classList.add('player--winner');
     player1.classList.remove('player--active');
-    player2.classList.add('player--active');
+    player2.classList.remove('player--active');
   }
 };
 
@@ -54,7 +49,28 @@ const sumTotalScore = function () {
   }
 };
 
+const init = function () {
+  curScore = 0;
+  totalScore1 = 0;
+  totalScore2 = 0;
+
+  dice.classList.add('hidden');
+
+  currentScoreP1.textContent = curScore;
+  currentScoreP2.textContent = curScore;
+  totalScoreP1.textContent = totalScore1;
+  totalScoreP2.textContent = totalScore2;
+
+  player1.classList.remove('player--winner');
+  player2.classList.remove('player--winner');
+  player1.classList.add('player--active');
+  player2.classList.remove('player--active');
+  dice.classList.add('hidden');
+};
+
 //METHODS
+init();
+
 document.querySelector('.btn--roll').addEventListener('click', function () {
   if (totalScore1 < 100 && totalScore2 < 100) {
     const randDice = Math.trunc(Math.random() * 6) + 1;
@@ -80,19 +96,4 @@ document.querySelector('.btn--hold').addEventListener('click', function () {
   }
 });
 
-document.querySelector('.btn--new').addEventListener('click', function () {
-  curScore = 0;
-  totalScore1 = 0;
-  totalScore2 = 0;
-
-  currentScoreP1.textContent = curScore;
-  currentScoreP2.textContent = curScore;
-  totalScoreP1.textContent = totalScore1;
-  totalScoreP2.textContent = totalScore2;
-
-  player1.classList.remove('player--winner');
-  player2.classList.remove('player--winner');
-  player2.classList.remove('player--active');
-  player1.classList.add('player--active');
-  dice.classList.add('hidden');
-});
+document.querySelector('.btn--new').addEventListener('click', init);
